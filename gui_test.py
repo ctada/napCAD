919283d702554ccfe5d31fcd956049e460e433c2
ctada <celine.ta@students.olin.edu>
2015-04-06 15:10:19 -0400

    working with opencv, button

--- gui_test.py
+++ gui_test.py
@@ -4,18 +4,25 @@ Copied from http://kieleth.blogspot.com/2014/05/webcam-with-opencv-and-tkinter.h
 
 import Tkinter as tk
 import cv2
+import numpy as np
 from PIL import Image, ImageTk  # sudo pip install Pillow, sudo apt-get install python-imaging-tk
  
 width, height = 800, 600
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
- 
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+if not cap.isOpened(): 
+	cap.open()
+
 root = tk.Tk()
-root.bind('<escape>', lambda e: root.quit())
+#root.bind('<escape>', lambda e: root.quit())
 lmain = tk.Label(root)
 lmain.pack()
- 
+
+def callback():
+    print "click!"
+
 def show_frame():
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
@@ -25,6 +32,9 @@ def show_frame():
     lmain.imgtk = imgtk
     lmain.configure(image=imgtk)
     lmain.after(10, show_frame)
- 
+    
+b = tk.Button(root, text="OK", command=callback)
+b.pack()
+
 show_frame()
 root.mainloop()
