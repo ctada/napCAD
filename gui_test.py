186c0549f390f2c5cca21765af3800f8403bd495
ctada <celine.ta@students.olin.edu>
2015-04-12 16:00:34 -0400

    shows matplotlib render of 3D form

--- gui_test.py
+++ gui_test.py
@@ -8,7 +8,13 @@ import cv2
 import numpy as np
 from PIL import Image, ImageTk  # sudo pip install Pillow, sudo apt-get install python-imaging-tk
 import stl_test
- 
+import matplotlib, sys
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+# implement the default mpl key bindings
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+
 width, height = 800, 600
 cap = cv2.VideoCapture(0)
 #cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
@@ -18,6 +24,7 @@ if not cap.isOpened():
 	cap.open()
 
 root = tk.Tk()
+root.title('napCAD')
 
 #root.bind('<escape>', lambda e: root.quit())
 lmain = tk.Label(root)
@@ -31,16 +38,29 @@ def callback():
     frame = cv2.flip(frame, 1)
     curFrame = frame
     cv2.imwrite("napSketch.jpg",curFrame)
-    print "Photo Taken"
+    print "Photo taken"
+
     #get calculated point things
     x = [0,0,0.5,1,1]
     y = [0,1,0.5,0,1]
     z = [0,0,1,0,0]
     #triangulate
     stl = stl_test.triangulation(x,y,z)
-    #stl_test.tri_vis(x,y,z)
-    save_as(stl) #save STL instead of text
+    fig = stl_test.tri_vis(x,y,z)
 
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.show()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    toolbar = NavigationToolbar2TkAgg( canvas, root )
+    toolbar.update()
+    canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    canvas.mpl_connect('key_press_event', on_key_event)
+
+    #q = tk.Button(master=root, text='Quit', command=_quit).pack(side=tk.BOTTOM)
+    save_as(stl) #save STL instead of text
+    #_quit
+    
 def show_frame():
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
@@ -60,9 +80,18 @@ def save_as(content):
         filetypes = (("napCAD STL", "*.stl"),("testText", ".txt")), 
         title="Save STL as...")    #in the save function
     stl_test.stl_write(f,content)
+    #root.quit()
+def _quit():
+    root.quit()     # stops mainloop
+    root.destroy()  # this is necessary on Windows to prevent
+                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+def on_key_event(event):
+    print('you pressed %s'%event.key)
+    key_press_handler(event, canvas, toolbar)
 
 #button_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 5}
 b = tk.Button(root, text ="Convert to STL", command = callback).pack()
+q = tk.Button(master=root, text='Quit', command=_quit).pack(side=tk.BOTTOM)
 
 show_frame()
 root.mainloop()
