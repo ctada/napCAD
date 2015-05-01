c333651804ff6819dfeec957df2ad50b6a4c7d61
Caleb Kissel <caleb.kissel@students.olin.edu>
2015-05-01 10:10:35 -0400

    fixed bug

--- integrationtest.py
+++ integrationtest.py
@@ -6,8 +6,10 @@ def napCAD_main():
 	testshapeSquare=[(1,0),(2,0),(2,1),(3,1),(3,2),(2,2),(2,3),(1,3),(1,2),(0,2),(0,1),(1,1)]
 	foldlinesSquare=[[(1,1),(2,1)],[(2,1),(2,2)],[(2,2),(1,2)],[(1,2),(1,1)]]
 
+	# testshapePyramid=
+
 
 
 	faces=face_finder(testshapeSquare,foldlinesSquare)
-	shape3D=folding_v2(faces[0],faces[1])
+	shape3D=make_dictionaries(faces[0],faces[1])
 	return shape3D
